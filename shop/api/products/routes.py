40a3369f3a7d504a14910865e8cc30e9a0810125
GhostFanta <huiyueguan111@gmail.com>
2020-02-09 23:03:30 -0500

    Review routes

--- shop/api/products/routes.py
+++ shop/api/products/routes.py
@@ -19,7 +19,7 @@ use_args = parser.use_args
 def get_products():
     products = Product.query.all()
     if not products:
-        return ProductException.no_products_in_db(),HTTPStatus.NOT_FOUND
+        return ProductException.no_products_in_db(), HTTPStatus.NOT_FOUND
     return jsonify(products_schema.dump(products)), HTTPStatus.OK
 
 
@@ -58,22 +58,25 @@ def delete_product(product_id):
 
 @products_blueprint.route('/products/<product_id>/product_reviews', methods=['GET'])
 def get_product_view_by_product_id(product_id):
-    reviews = ProductReview.query.filter(product_id=product_id)
+    reviews = ProductReview.query.filter_by(product_id=product_id).all()
     if not reviews:
         return ProductException.no_product_review(), HTTPStatus.NOT_FOUND
-    return product_reviews_schema.dump(reviews), HTTPStatus.OK
+    return jsonify(product_reviews_schema.dump(reviews)), HTTPStatus.OK
 
 
 @products_blueprint.route('/products/<product_id>/product_reviews', methods=['POST'])
 def create_product_view(product_id):
     data = request.json
-    product_review = ProductReview.create(**data)
-    return product_review_schema.dump(product_review), HTTPStatus.OK
+    product = Product.query.get(product_id)
+    if not product:
+        return ProductException.product_not_exist(), HTTPStatus.NOT_FOUND
+    product_review = ProductReview.create(**data, product_id=product_id)
+    return jsonify(product_review_schema.dump(product_review)), HTTPStatus.OK
 
 
 @products_blueprint.route('/products/<product_id>/product_reviews/<review_id>', methods=['GET'])
 def get_product_view_by_product_id_and_review_id(product_id, review_id):
     reviews = ProductReview.query.filter(product_id=product_id, id=review_id)
     if not reviews:
-        return ProductException.no_product_review(),HTTPStatus.NOT_FOUND
+        return ProductException.no_product_review(), HTTPStatus.NOT_FOUND
     return product_reviews_schema.dump(reviews), HTTPStatus.OK
